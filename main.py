7dae65258990fc1f6b29574711fd73c51c236030
Grebbinz <56519447+Grebbinz@users.noreply.github.com>
2022-04-04 19:36:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-message = input('Message: ')
-key = int(input('Key: '))
-
+# Encrypts or decrypts a line of text
 
 def encrypt(message, key):
 	encrypted_message = ''
 
 	for i in message:
-		encrypted_message = encrypted_message + chr(ord(i) + key)
+		encrypted_value = ord(i) + key
+
+		while encrypted_value < 32:
+			encrypted_value += 94
+
+		while encrypted_value > 127:
+			encrypted_value -= 94
+
+		encrypted_message = encrypted_message + chr(encrypted_value)
 
 	return encrypted_message
 
@@ -15,10 +21,21 @@ def decrypt(message, key):
 	decrypted_message = ''
 
 	for i in message:
-		decrypted_message = decrypted_message + chr(ord(i) - key)
+		decrypted_value = ord(i) - key
+
+		while decrypted_value < 32:
+			decrypted_value += 94
+
+		while decrypted_value > 127:
+			decrypted_value -= 94
+
+		decrypted_message = decrypted_message + chr(decrypted_value)
 
 	return decrypted_message
 
+
+message = input('Message: ')
+key = int(input('Key: '))
 
 choice = input('encrypt or decrypt? ')
 
